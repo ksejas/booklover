c33ef1ba510d166373f1338a46bd56c45704a3e8
ksejas <111703117+ksejas@users.noreply.github.com>
2022-10-29 21:10:19 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='booklover',
+      version='0.1',
+      description='A package that helps you track the books you read and your review of them.',
+      url='https://github.com/ksejas/booklover',
+      author='Avid Reader',
+      author_email='booklover@example.com',
+      license='MIT',
+      packages=['booklover'])
